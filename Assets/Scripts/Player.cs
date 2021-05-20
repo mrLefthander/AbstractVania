6dc8d74c057a05c06784928afa0eabbda863946d
Lefthander <lefthander89@gmail.com>
2021-05-20 22:19:05 +0300

    Running animation when Running

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Player : MonoBehaviour
 {
   [SerializeField] 
   private float runSpeed = 5f;
+
+  bool isAlive = true;
+
   private Rigidbody2D myRigidBody;
+  private Animator myAnimator;
+  private SpriteRenderer mySpriteRenderer;
 
   void Start()
   {
     myRigidBody = GetComponent<Rigidbody2D>();
+    myAnimator = GetComponent<Animator>();
+    mySpriteRenderer = GetComponent<SpriteRenderer>();
   }
 
  
@@ -23,5 +32,15 @@ public class Player : MonoBehaviour
     float controlThrow = Input.GetAxis("Horizontal");
     Vector2 playerVolecity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
     myRigidBody.velocity = playerVolecity;
+
+    bool isPlayerRunning = Mathf.Abs(controlThrow) > Mathf.Epsilon;
+    myAnimator.SetBool("isRunning", isPlayerRunning);
+    FlipSprite(controlThrow);
+  }
+
+  private void FlipSprite(float controlThrow)
+  {
+    bool isRunningLeft = controlThrow < 0;
+    mySpriteRenderer.flipX = isRunningLeft;
   }
 }
